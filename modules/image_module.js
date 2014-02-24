c78b8a65fa927fc73520ad17aea3375ab1240175
Edgar <edgar@learningdata.ie>
2014-02-24 16:00:46 +0000

    fixed getImage path

--- modules/image_module.js
+++ modules/image_module.js
@@ -2,7 +2,7 @@ var request = require("request");
 
 var ImageModule = {
   getImage: function(url, filename){
-    request(img_url).pipe(fs.createWriteStream("/tmp/" + filename));
+    request(url).pipe(fs.createWriteStream("/tmp/" + filename));
   }
 }
 
