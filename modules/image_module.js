00b90ad57c2189d7e27ae398f6d949bf5c110300
Edgar <edgar@learningdata.ie>
2014-02-24 21:00:13 +0000

    created method to resize the image

--- modules/image_module.js
+++ modules/image_module.js
@@ -1,8 +1,13 @@
 var request = require("request");
+var fs = require("fs");
+var image = require("imagemagick");
 
 var ImageModule = {
   getImage: function(url, filename){
     request(url).pipe(fs.createWriteStream("/tmp/" + filename));
+  },
+  resizeImage: function(pathImage, name, size) {
+    image.resize({srcPath: pathImage, dstPath: "/tmp/thumb/"+name, width: 200});
   }
 }
 
