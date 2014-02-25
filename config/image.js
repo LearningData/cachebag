50725ec40b1634c41b387efd230bd94bd7211b2e
Edgar <edgar@learningdata.ie>
2014-02-25 15:56:40 +0000

    added config file to images

---
+++ config/image.js
@@ -0,0 +1,5 @@
+exports.image_config = {
+    folder: "/Projects/cachebag/images/",
+    small: {"width": 200, "height": 200, "folder": "/Projects/cachebag/images/thumb/"},
+    medium: {"width": 400, "height": 400}
+};
