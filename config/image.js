0caea3c1ffb9ccbdc53ecaf1b9c24627b281d049
Edgar <edgar@learningdata.ie>
2014-02-25 16:01:13 +0000

    changed the indentation of configs

--- config/image.js
+++ config/image.js
@@ -1,5 +1,12 @@
 exports.image_config = {
     folder: "/Projects/cachebag/images/",
-    small: {"width": 200, "height": 200, "folder": "/Projects/cachebag/images/thumb/"},
-    medium: {"width": 400, "height": 400}
+    small: {
+      "width": 200,
+      "height": 200,
+      "folder": "/Projects/cachebag/images/thumb/"
+    },
+    medium: {
+      "width": 400,
+      "height": 400
+    }
 };
