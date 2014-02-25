371c90d76d206da2b393c97fd4da3ee91a1c84b1
Edgar <edgar@learningdata.ie>
2014-02-25 16:03:39 +0000

    added path of medium images

---
+++ modules/resourcebag.js
@@ -0,0 +1,11 @@
+var request = require("request");
+
+exports.Resource = {
+  getInfo: function(url, callback) {
+    request(url, function(err, response, body){
+      if (err) { return callback("Error to get info")};
+      var info = JSON.parse(body);
+      return callback(info);
+    });
+  }
+}
