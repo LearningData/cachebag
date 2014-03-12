46d61251e7c0f701e8145b3236e3c507839500cf
Edgar <edgar@learningdata.ie>
2014-03-12 13:02:04 +0000

    refactor on download method

--- routes/images.js
+++ routes/images.js
@@ -1,22 +1,28 @@
 var fs = require("fs");
 var ImageModule = require("../modules/image_module.js").ImageModule;
 var image_config = require("../config/image.js").image_config;
-var request = require("request");
-var image = require("imagemagick");
 
 exports.download = function(req, res) {
   var config = image_config[req.params.size];
   var filename = config.folder + req.params.id;
 
-  fs.readFile(filename , function (err, data) {
-    if (err) {
+  fs.exists(filename, function(exists){
+    if(!exists){
       console.log("Saving new image: " + req.params.id);
 
-      ImageModule.saveImage(req.params.id, req.params.size, function(data){
-         res.end(data, "binary");
+      ImageModule.saveImage(req.params.id, req.params.size, function(err, data){
+          if(err) {
+            res.end("Error save image");
+            return;
+          }
       });
     }
-    console.log("Getting new image: " + filename);
-    res.end(data, "binary");
+
+    fs.readFile(filename, function(err, data){
+      if(err) {
+        console.log(err);
+      }
+      res.end(data, "binary");
+    });
   });
 };
