4156398bac7485a03ebd1dd71bf2877eaa9f4afd
Edgar <edgar@learningdata.ie>
2014-06-17 09:19:34 +0100

    changed the server on deploy script

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,17 +1,17 @@
 set :stage, :staging
 
-role :web, %w{azureuser@resourcebag.cloudapp.net}
+role :web, %w{azureuser@cachebag.cloudapp.net}
 
 # set :ssh_options, { port: 3536, forward_agent: true, auth_methods: %w(password)}
 
 # server 'azureuser@137.116.242.92 -p', user: 'azureuser', roles: %w{web}
 
 
-server 'resourcebag.cloudapp.net',
+server 'cachebag.cloudapp.net',
   user: 'azureuser',
   roles: %w{app},
   ssh_options: {
-    port: 3537,
+    port: 3535,
     user: 'azureuser',
     forward_agent: false,
     keys: [File.join(ENV["HOME"], "Projects/keys", "azure.key")]
